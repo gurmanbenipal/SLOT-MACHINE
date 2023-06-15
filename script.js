d2409dd932c6bb8bb7d736f2db9c33691f011e4d
gurmanbenipal <gurmxnbenipal@gmail.com>
2023-06-15 14:01:43 -0700

    added some more notes in js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// First thing to do is get the spin function working 
+// Use three math randoms 
+// Use the timer thing to make it spin wait 
+// First I wouid get the basic code working and then I’ll focus on the design and other functions 
+
+// Winning combos can be
+
+// 111
+// 000
+// 222
+
+// Every number will have a colour or symbol to it that display on each turn
+
+
 // define variables for ths slot machine
 // there will be one player
 //there will be some winning combos thatll help the player win
@@ -5,3 +19,5 @@
 //the button will be able to start , spin, cash out and buy coins
 //to make the lights flash ill be using one count down method as we learned in class
 // functions will be used to handle most of the work as itll make it easier and more efficient
+
+
