bed00734f912760ee3ca4cf85732b87089bd7f36
gurmanbenipal <gurmxnbenipal@gmail.com>
2023-06-14 23:06:22 -0700

    add files

---
+++ script.js
@@ -0,0 +1,7 @@
+// define variables for ths slot machine
+// there will be one player
+//there will be some winning combos thatll help the player win
+//the slot machine will spin will random number so i can use mathRandom
+//the button will be able to start , spin, cash out and buy coins
+//to make the lights flash ill be using one count down method as we learned in class
+// functions will be used to handle most of the work as itll make it easier and more efficient
