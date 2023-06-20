3954b4381678b8a74e423930692ab7bfe27bf0fb
gurmanbenipal <gurmxnbenipal@gmail.com>
2023-06-20 16:07:57 -0700

    updated

--- script.js
+++ script.js
@@ -1,23 +1,111 @@
-// First thing to do is get the spin function working 
-// Use three math randoms 
-// Use the timer thing to make it spin wait 
-// First I wouid get the basic code working and then I’ll focus on the design and other functions 
-
-// Winning combos can be
-
-// 111
-// 000
-// 222
-
-// Every number will have a colour or symbol to it that display on each turn
-
-
-// define variables for ths slot machine
-// there will be one player
-//there will be some winning combos thatll help the player win
-//the slot machine will spin will random number so i can use mathRandom
-//the button will be able to start , spin, cash out and buy coins
-//to make the lights flash ill be using one count down method as we learned in class
-// functions will be used to handle most of the work as itll make it easier and more efficient
+const box0 = document.getElementById('box0');
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const wallet = document.querySelector('span');
+const resultsEl = document.querySelector('h2');
+const resetEl = document.querySelector('.reset');
+const playBtn = document.querySelector('.start');
+const cashOut = document.querySelector('.cash-out');
+const icons = [
+    'icons/apple.png',
+    'icons/cherries.png',
+    'icons/cherry.png',
+    'icons/lemon.png',
+    'icons/orange.png',
+    'icons/seven.png',
+    'icons/watermelon.png'
+];
 
 
+
+let game = true;
+
+function wonOrTryAgain() {
+    
+    let winningCombo = generateRandomWinningCombo();
+
+    function generateRandomWinningCombo() {
+      return [
+        Math.floor(Math.random() * 3),
+        Math.floor(Math.random() * 3),
+        Math.floor(Math.random() * 3)
+      ];
+    }
+     
+    function randomSpinCombo(){
+        return [
+            Math.floor(Math.random() * 3),
+            Math.floor(Math.random() * 3),
+            Math.floor(Math.random() * 3)
+          ];
+    }
+
+  let randomCombo = randomSpinCombo()
+  let r = Math.floor(Math.random() * 7);
+  let p = Math.floor(Math.random() * 7);
+  let b = Math.floor(Math.random() * 7);
+
+
+  if (randomCombo[0] === winningCombo[0] && randomCombo[1] === winningCombo[1] && randomCombo[2] === winningCombo[2] && game === true) {
+    box0.style.backgroundImage = `url(${icons[r]})`;
+    box1.style.backgroundImage = `url(${icons[r]})`;
+    box2.style.backgroundImage = `url(${icons[r]})`;
+    resultsEl.innerText = 'JACKPOT!';
+    wallet.innerHTML = parseInt(wallet.innerHTML) + 1000;
+    game = false;
+    won = true;
+    playBtn.disabled = true;
+  }
+
+   else {
+    if (game === true) {
+        wallet.innerHTML = parseInt(wallet.innerHTML) - 10;
+    let randomIcons = generateRandomIcons(icons, r, p, b);
+      box0.style.backgroundImage = `url(${randomIcons[0]})`;
+      resultsEl.innerText = 'TRY AGAIN';
+      box1.style.backgroundImage = `url(${randomIcons[1]})`;
+      box2.style.backgroundImage = `url(${randomIcons[2]})`;
+    }
+  }
+}
+
+function generateRandomIcons(icons, r, p, b) {
+    let randomIcons = [icons[r], icons[p], icons[b]];
+  
+    // Check if all icons are the same
+    while (randomIcons[0] === randomIcons[1] && randomIcons[1] === randomIcons[2]) {
+      r = Math.floor(Math.random() * 7);
+      p = Math.floor(Math.random() * 7);
+      b = Math.floor(Math.random() * 7);
+      randomIcons = [icons[r], icons[p], icons[b]];
+    }
+  
+    return randomIcons;
+  }
+
+function resetButton() {
+  game = true;
+  playBtn.disabled = false;
+  resultsEl.innerText = 'LETS PLAY';
+  box0.style.backgroundImage = 'none';
+  box1.style.backgroundImage = 'none';
+  box2.style.backgroundImage = 'none';
+}
+
+playBtn.addEventListener('click', function() {
+  setTimeout(function() {
+    wonOrTryAgain();
+    
+    playBtn.disabled = false; // Enable the spin button
+    
+}, 1000);
+
+  playBtn.disabled = true; // Disable the spin button during the timer
+});
+
+resetEl.addEventListener('click', resetButton);
+
+
+cashOut.addEventListener('click',function(){
+    wallet.innerHTML = '0';
+})
