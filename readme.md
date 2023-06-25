b1436ff7c460e78c79670e2227264d7fa9f74c95
gurmanbenipal <gurmxnbenipal@gmail.com>
2023-06-25 16:50:16 -0700

    updated the js,css,html and the read.me file

--- readme.md
+++ readme.md
@@ -1,24 +1,141 @@
-// First thing to do is get the spin function working 
-// Use three math randoms 
-// Use the timer thing to make it spin wait 
-// First I wouid get the basic code working and then I’ll focus on the design and other functions 
+# Las Vegas Slot Machine Game
 
-// Winning combos can be
+Welcome to my Slot Machine Game :).This game is a browser based version for a real life slot machine game which lets you enjoy the in game experience from just your browser.
 
-// 111
-// 000
-// 222
+## Features
+- Place your bets for a win!
+- While playing enjoy beautiful animations.
+- Modify your bet amount for big wins.
+- See live results of your winnings and losses.
+- Enjoy sound effects and background music.
 
-// Every number will have a colour or symbol to it that display on each turn
+## Technologies Used
+<img src="html-5.png" alt="HTML5" width="50" height="45">
+<img src="css-3.png" alt="CSS" width="50" height="45">
+<img src="js.png" alt="JS" width="35" height="35">
+<img src="Visual_Studio_Code_1.35_icon.svg.png" alt="VS CODE" width="35" height="35">
+<img src="github-sign.png" alt="GIT HUB" width="35" height="35">
+
+## Getting Started
+
+1. Click the link to play: `https://gurmanbenipal.github.io/SLOT-MACHINE/`
+2. Place your bets to start spinning!
+
+## Game Instructions
+
+1. Set your bet and press the place bet button to start. Starting Bets are $10 by default
+2. Coin balance is $100 at every start or after reset by default.
+3. Wait for the boxes to stop glowing after you place your bet.
+4. If the game icons match, you win!. Otherwise you can try again if your coin balance is $10 or more.
+5. You will see your coin balance live, so place your bets accordingly..
+6. At any point you can choose to cash out your remaining coins if you like. However know that the cash out feature wont be available while the spiining is active.
+7. Every win amount for each row, column or a diagonal will be the double of your betting amount and for every turn you loose, you will loose your exact betting amount.
+8. Incase you ever run out of money or end up cashing out, you will be asked to reset the game in order to play again.
+9. You can enjoy the background music by clicking on the MUSIC ON/OFF button. If its on, a slider will be displayed for you to adjust the volume levels.
+10. Go have fun and aim for the jackpot!!
+
+## Screenshots
+
+## Start game or reset game
+<img src="Screenshot 2023-06-25 at 1.31.17 PM.png" alt="lets play" width="500" height="400">
+
+## Spinning
+<img src="Screenshot 2023-06-25 at 2.22.47 PM.png" alt="spinning" width="500" height="400">
+
+## Win situation
+<img src="Screenshot 2023-06-25 at 2.26.42 PM.png" alt="you won" width="500" height="400">
+
+## Lose situation 
+<img src="Screenshot 2023-06-25 at 2.23.10 PM.png" alt="you lost" width="500" height="400">
+
+## Out of all coins
+<img src="Screenshot 2023-06-25 at 4.38.56 PM.png" alt="you're out of money" width="500" height="400">
+
+## Note! A jackpot is very rare and almost impossible to achieve
+<img src="Screenshot 2023-06-25 at 2.36.07 PM.png" alt="JACKPOT" width="500" height="400">
 
 
-// define variables for ths slot machine
-// there will be one player
-//there will be some winning combos thatll help the player win
-//the slot machine will spin will random number so i can use mathRandom
-//the button will be able to start , spin and cash out 
 
 
-BONUS FUNTIONS:
-To make the lights flash ill be using one count down method as we learned in class
-Functions will be used to handle most of the work as itll make it easier and more efficient
+
+## CODE PREVIEW! (WIN OR LOSE)
+```javascript
+ 
+ function checkWin(box1, box2, box3) {
+    if (box1.style.backgroundImage === box2.style.backgroundImage &&
+        box2.style.backgroundImage === box3.style.backgroundImage) {
+        box1.classList.add('win');
+        box2.classList.add('win');
+        box3.classList.add('win'); 
+        wallet.innerHTML = parseInt(wallet.innerHTML) + (parseInt(betAmount.innerText)*2);
+        return true;
+    }
+    return false;
+}
+function winOrLoose() {
+let tracker = 0;  
+let hasWon = false;
+let totalWinAmount = 0;
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].classList.remove('win');
+}
+
+  for (let i = 0; i < 9; i += 3) {
+
+    if (checkWin(boxes[i], boxes[i + 1], boxes[i + 2])) {
+          tracker++;
+          totalWinAmount += parseInt(betAmount.innerText) * 2;
+          hasWon = true;
+      }
+  }
+  for (let i = 0; i < 3; i++) {
+      if (checkWin(boxes[i], boxes[i + 3], boxes[i + 6])) {
+        tracker++;
+        totalWinAmount += parseInt(betAmount.innerText) * 2;  
+        hasWon = true;
+      }
+  }
+  if (checkWin(boxes[0], boxes[4], boxes[8])) 
+       {
+        tracker++;
+        totalWinAmount += parseInt(betAmount.innerText) * 2;
+        hasWon = true;
+  }
+  if(checkWin(boxes[2], boxes[4], boxes[6])){
+    tracker++;
+    totalWinAmount += parseInt(betAmount.innerText) * 2;
+    hasWon = true;
+  }
+  if(hasWon){
+    resultsEl.innerText = `CONGRATS!! 😊 YOU WON $${totalWinAmount}`;
+    jackpotSound()
+  }
+  if(tracker === 8){
+    resultsEl.innerText = `JACKPOTT!! 🥶 YOU WON $${totalWinAmount}`;
+  }
+  if (!hasWon) {
+    resultsEl.innerText = `TRY AGAIN 🙁 YOU LOST $${parseInt(betAmount.innerText)}`;
+      wallet.innerHTML = parseInt(wallet.innerHTML) - parseInt(betAmount.innerText);
+ }
+  if(wallet.innerHTML==='0'){
+    betAmount.innerText = '0'
+    betBtn.disabled= true;
+  }else{
+  betAmount.innerText = '10';}
+}
+
+```
+
+## Upcoming Features
+- Spinning wheel visuals
+- Switch between multiple languages
+- Bonus rounds
+
+
+## Images and sound credits
+
+- [Sounds and Background music ](https://splice.com/) : Splice.com is a subscription based website that lets you download royalty free samples.
+- [Fruit icons](https://www.flaticon.com/): Flaticon.com is a royalty free site for downloading amazing icons.
+
+Please feel free to submit any bug reports. Hope you enjoy playing Las Vegas Slot Machine!.
--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-const box0 = document.getElementById('box0');
-const box1 = document.getElementById('box1');
-const box2 = document.getElementById('box2');
+
+//ALL THE ELEMENTS THAT I NEEDED TO GRAB FROM HTML TO MANIPULATE
+const boxes = [
+    document.getElementById('box0'),
+    document.getElementById('box1'),
+    document.getElementById('box2'),
+    document.getElementById('box3'),
+    document.getElementById('box4'),
+    document.getElementById('box5'),
+    document.getElementById('box6'),
+    document.getElementById('box7'),
+    document.getElementById('box8')
+  ];
 const wallet = document.querySelector('span');
 const resultsEl = document.querySelector('h2');
 const resetEl = document.querySelector('.reset');
 const playBtn = document.querySelector('.start');
 const cashOut = document.querySelector('.cash-out');
 const music = document.querySelector('.backgroundSound');
+const test = document.querySelector('.testing');
+const betBtn = document.querySelector('.increasebet');
+const betAmount = document.querySelector('.amount');
+const volumeSlider = document.getElementById('volume');
+const label = document.querySelector('label');
+const language = document.querySelector('.language');
+const body = document.querySelector('body');
+
+// THIS FUNCTION IS USED FOR THE BACKGROUND ANIMATION THATLL HAPPEN IF THE PLAYER LOSES ALL THEIR MONEY
+function addBackgroundAnimation() {
+  body.classList.add('background-animation');
+}
+
+
+function removeBackgroundAnimation() {
+  body.classList.remove('background-animation');
+}
+
+
+//ALL THE ICONS THAT'LL BE DISPLAYED IN THE SLOT MACHINE
 const icons = [
     'icons/apple.png',
     'icons/cherries.png',
@@ -17,118 +47,284 @@ const icons = [
     'icons/watermelon.png'
 ];
 
-
-
+// THIS VARIABLE WILL HELP SHUTTING OFF OR STARTING THE GAME AGAIN
 let game = true;
-
-function wonOrTryAgain() {
-   
-    let winningCombo = generateRandomWinningCombo();
-
-    function generateRandomWinningCombo() {
-      return [
-        Math.floor(Math.random() * 3),
-        Math.floor(Math.random() * 3),
-        Math.floor(Math.random() * 3)
-      ];
-    }
-     
-    function randomSpinCombo(){
-        return [
-            Math.floor(Math.random() * 3),
-            Math.floor(Math.random() * 3),
-            Math.floor(Math.random() * 3)
-          ];
-    }
-
-  let randomCombo = randomSpinCombo()
-  let r = Math.floor(Math.random() * 7);
-  let p = Math.floor(Math.random() * 7);
-  let b = Math.floor(Math.random() * 7);
-
-
-  if (randomCombo[0] === winningCombo[0] && randomCombo[1] === winningCombo[1] && randomCombo[2] === winningCombo[2] && game === true) {
-    box0.style.backgroundImage = `url(${icons[r]})`;
-    box1.style.backgroundImage = `url(${icons[r]})`;
-    box2.style.backgroundImage = `url(${icons[r]})`;
-    jackpotSound()
-    resultsEl.innerText = 'JACKPOT!';
-    wallet.innerHTML = parseInt(wallet.innerHTML) + 1000;
-   
-    
-  
-  }
-
-   else {
-    if (game === true) {
-        wallet.innerHTML = parseInt(wallet.innerHTML) - 10;
-    let randomIcons = generateRandomIcons(icons, r, p, b);
-      box0.style.backgroundImage = `url(${randomIcons[0]})`;
-      resultsEl.innerText = 'TRY AGAIN';
-      resultsEl.style.color = 'rgb(123, 0, 255)';
-      box1.style.backgroundImage = `url(${randomIcons[1]})`;
-      box2.style.backgroundImage = `url(${randomIcons[2]})`;
-    }
+//This will make sure the reset button doesnt work during an active game, itll only work if game is over or after you cash out
+resetEl.disabled = true
+startIcons()
+//THIS FUNCTION WILL GIVE US THE START ICONS, THEY WILL ALL BE THE SAME JUST TO GIVE A NICE LOOK TO THE START PAGE
+function startIcons(){
+  const randomIndex = Math.floor(Math.random() * icons.length);
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.backgroundImage = `url(${icons[randomIndex]})`;
   }
 }
 
-function generateRandomIcons(icons, r, p, b) {
-    let randomIcons = [icons[r], icons[p], icons[b]];
-  
-    // Check if all icons are the same
-    while (randomIcons[0] === randomIcons[1] && randomIcons[1] === randomIcons[2]) {
-      r = Math.floor(Math.random() * 7);
-      p = Math.floor(Math.random() * 7);
-      b = Math.floor(Math.random() * 7);
-      randomIcons = [icons[r], icons[p], icons[b]];
+//THIS FUNCTION HAS A LOOP RUNNING , THAT WILL RUN UNTIL ALL THE BOXES ARE FILLED WITH RANDOM ICONS
+function spreadIcons() {
+    for (let i = 0; i < boxes.length; i++) {
+      const randomIndex = Math.floor(Math.random() * icons.length);
+      boxes[i].style.backgroundImage = `url(${icons[randomIndex]})`;
     }
-  
-    return randomIcons;
   }
+//THIS PLAYBTN IS USED TO CALL THE SPREAD ICONS FUNCTION,WINORLOOSE FUNCTION AND SETTIMEOUT EACH TIME ITS CLICKED
+playBtn.addEventListener('click', function() {
+  stopGlowing();
+  boxesGlow()
+
+  removeBoxesImage()
+  resultsEl.innerText = "SPINNING...";
+  resultsEl.classList.add('textanimation');
+  playBtn.classList.add('spinanimation');
+    playBtn.disabled = true; // Disable the spin button
+    betBtn.disabled = true;
+    cashOut.disabled = true; //Disable the cash out button while we are spinnin so the player cant run away with the money while its spinning 
+    setTimeout(function() {
+      joinFunctions();
+      stopBoxesGlow()
+      
+      resultsEl.classList.remove('textanimation');
+      playBtn.classList.remove('spinanimation');
+      playBtn.disabled = !game; // this will enable the spin button if the game is not over
+      if(parseInt(wallet.innerHTML) > parseInt(betAmount.innerText)){betBtn.disabled = false;}//THIS WILL JUST MAKE SURE THAT THE BET BUTTON DOESNT COME UP IN CASE THE COIN VALUE DROPS RIGHT AFTER THE SPINNING
+      cashOut.disabled = false;
+      gameOver();
+  }, 1800);
+  }
+);
 
 
-function resetButton() {
-  game = true;
-  wallet.innerHTML = '100';
-  playBtn.disabled = false;
-  resetEl.disabled = true;
-  resultsEl.innerText = 'LETS PLAY';
-  cashOut.disabled = false;
-  box0.style.backgroundImage = 'none';
-  box1.style.backgroundImage = 'none';
-  clickSound()
-  box2.style.backgroundImage = 'none';
+//THIS FUNCTION JOINS WINORLOOSE AND SPREADICONS FUNCTION 
+function joinFunctions(){
+ //THE FUNCTION BELOW IS JUST THERE IN CASE YOU WANT TO TEST THE JACKPOT FEATURE, WHICH IS A RARE SCENERIO, PROBABLY IMPOSSIBLE TO HAPPEN
+ //BUT ALL YOU GOTTA DO IS JUST UNCOMMENT THE FUNCTION BELOW AND THEN COMMENT OUT SPREADICONS FUNCTION, ONCE YOUR DONE YOU CAN PUT EVERYTHING BACK TO HOW IT WAS
+  // startIcons()
+  spreadIcons() 
+    winOrLoose()
 }
 
-
-
-
-
-
-
-resetEl.addEventListener('click', resetButton);
-
-resetEl.disabled = true;
-cashOut.addEventListener('click',function(){
-    resultsEl.innerText = `YOU CASHED OUT $${wallet.innerHTML}! RESET TO PLAY AGAIN:)`;
-    wallet.innerHTML = '0';
-})
-function loseSound(){
-    const loseSound = new Audio('sounds/ESM_Death_Game_Over_1_Sound_FX_Arcade_Casino_Kids_Mobile_App.mp3')
-    loseSound.play();
-}
-
-function spinSound() {
-    const spinSound = new Audio('sounds/CasinoSlotMachine_S08SP.184.mp3'); 
-    spinSound.play();
-}
+//SOUNDS BELOW
 function jackpotSound() {
-    const jackpotSound = new Audio('sounds/ESM_Casino_Bling_Sound_FX_Arcade_Kids_Mobile_App.mp3'); 
-    jackpotSound.play();
+  const jackpotSound = new Audio('sounds/ESM_Casino_Bling_Sound_FX_Arcade_Kids_Mobile_App.mp3'); 
+  jackpotSound.play();
 }
-function cashOutSound() {
-    resetEl.disabled = false;
-    playBtn.disabled = true;
+function loseSound(){
+  const loseSound = new Audio('sounds/ESM_Death_Game_Over_1_Sound_FX_Arcade_Casino_Kids_Mobile_App.mp3')
+  loseSound.play();
+}
+function clickSound() {
+  const clickSound = new Audio('sounds/MouseClick_SFXB.4113.mp3'); 
+  clickSound.play();
+}
+function spinSound() {
+  const spinSound = new Audio('sounds/CasinoSlotMachine_S08SP.184.mp3'); 
+  spinSound.play();
+}
+function betSound() {
+  const betSound = new Audio('sounds/SlotMachine_AP1.1310.mp3'); 
+  betSound.play();
+}
+function resetSound() {
+  const resetSound = new Audio('sounds/resetsoundtouse.mp3'); 
+  resetSound.play();
+}
+function musicButtonClickforSpin() {
+  clickSound();
+  spinSound();
+}
+playBtn.addEventListener('click',musicButtonClickforSpin);
+
+
+const musicSound = new Audio('sounds/CasinoSlotMachine_SFXB.21.mp3'); 
+  musicSound.loop = true;
+  function playMusicSound() {
+      
+    musicSound.play();
+}
+// THE FUNCTION WILL HANDLE ANY VOLUME CHANGES
+function volumeChange() {
+  musicSound.volume = volumeSlider.value / 100;
+}
+
+// THIS FUNCTION WILL HANDLE WHATEVER HAPPENS WHEN THE MUSIC ON/OFF BUTTON IS CLICKED
+//THIS FUNCTION WILL SIMPLY TURN ON AND OFF THE MUSIC BUTTON AND IT WILL ALSO SHOW US THE VOLUME SLIDER IF ITS ON AND THEN IF ITS OFF THEN THE SLIDER WILL DISSAPEAR
+function musicButtonClick() {
+  if (musicSound.paused) {
+    musicSound.play();
+    volumeSlider.style.display = "block"; 
+  } else {
+    musicSound.pause();
+    volumeSlider.style.display = "none"; 
+  }
+  clickSound();
+}
+
+music.addEventListener('click', musicButtonClick);
+volumeSlider.addEventListener('input', volumeChange);
+
+//THIS FUNCTION BELOW TAKES IN THREE PARAMETERS WHICH ARE JUST THE BOXES ELEMENTS
+//WE CAN CHANGE THE BOXES NUMBER LATER BUT ALL IT DOES IS IT CHECKS IF BOX1=BOX2 AND BOX2=BOX3
+//REMEMBER THAT WE CAN ALWAYS CHANGE THE BOX NUMBER BECAUSE ITS A PARAMETER
+
+  function checkWin(box1, box2, box3) {
+    if (box1.style.backgroundImage === box2.style.backgroundImage &&
+        box2.style.backgroundImage === box3.style.backgroundImage) {
+        box1.classList.add('win');
+        box2.classList.add('win');
+        box3.classList.add('win');
+    
+        //THE LINE BELOW WILL GIVE YOU COINS THATLL BE TWO TIMES YOUR BET AMOUNT FOR EACH WIN, NOW IF YOU FOR TWO OR MORE IN ONE TRY THEN IT WILL ADD THAT AS WELL
+        
+        wallet.innerHTML = parseInt(wallet.innerHTML) + (parseInt(betAmount.innerText)*2);
+        return true;
+    }
+//we are using return true or false here because we are using a if statement to check the conditions in this function below,
+//so this function will return a true if its met and if not itll say false. true will execute the if statement and false wont
+    return false;
+}
+//INSTEAD OF USING THE ABOVE FUNCTION  WE COULDVE ALSO JUST ADDED THE IF STATEMENT IN THE LOOPS BELOW BUT 
+//THAT WOULD BE A LOT OF CODE SO ITS BETTER TO USE THIS FUNCTION INSTEAD
+
+
+
+//THIS FUNCTION WILL CHECK IF YOU WIN OR DONT
+function winOrLoose() {
+//THIS VARIABLE IS JUST HERE SO WE CAN USE IT LATER TO DECLARE A LOOSE SITUATION
+//RIGHT NOW ITS FALSE BECAUSE THERES NO WIN YET 
+let tracker = 0;  
+let hasWon = false;
+let totalWinAmount = 0;
+//THIS FOR LOOP IS JUST HERE SO IT CAN REMOVE THE NEW CSS CLASS THAT WE ADD IN CHECKWIN AFTER EACH TURN
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].classList.remove('win');
+}
+
+  //FIRST LETS DEAL WITH THE ROWS, WE HAVE A FOR LOOP RUNNING THAT WILL CHECK EACH ROW ONCE
+  //EACH ROW BECAUSE IT HAS (I+3)  SINCE FIRST I IS 0 ITLL CHECK THE FIRST ROW USING THE IF STATEMENT BELOW
+  //AND AFTER IT WILL JUMP TO 3 AND DO THE SAME IF STATEMENT, FOLLOWED BY GOING TO 6
+  for (let i = 0; i < 9; i += 3) {
+  //THE IF STATEMENT HERE CHECKS THE BOX1, BOX2 AND BOX3 USING THE CHECKWIN FUNCTION ABOVE
+  //AFTER CHECKING IT WILL RUN TO BOX3... FOLLOWED BY BOX6...
+    if (checkWin(boxes[i], boxes[i + 1], boxes[i + 2])) {
+          tracker++;
+          totalWinAmount += parseInt(betAmount.innerText) * 2;
+          hasWon = true;
+      }
+  }
+
+  // SAME AS ABOVE BUT SINCE WE ARE DOING COLUMNS NOW, WE CAN USE THE IF STATEMENT
+  //TO JUST ADD 3 BECAUSE ADDING 3 TO 0 WILL PUSH US TO BOX3 AND THEN ADD 6 TO BOX0 
+  //WHICH PUSHES US TO BOX6, JUST LIKE ABOVE ITLL USE THE CHECKWIN FUNCTION
+  for (let i = 0; i < 3; i++) {
+      if (checkWin(boxes[i], boxes[i + 3], boxes[i + 6])) {
+        tracker++;
+        totalWinAmount += parseInt(betAmount.innerText) * 2;  
+        hasWon = true;
+      }
+  }
+
+  // FOR DIAGNOLS WE USE A SLIGHTLY DIFFERENT METHOD, WE WILL CHOOSE THE BOXES INDIVISUALLY
+  //COULDVE ALSO TRIED A FOR LOOP BUT IT WAS EASIER THIS WAY
+  if (checkWin(boxes[0], boxes[4], boxes[8])) 
+       {
+        tracker++;
+        totalWinAmount += parseInt(betAmount.innerText) * 2;
+        hasWon = true;
+  }
+  if(checkWin(boxes[2], boxes[4], boxes[6])){
+    tracker++;
+    totalWinAmount += parseInt(betAmount.innerText) * 2;
+    hasWon = true;
+  }
+  //I ADDED THE JACKPOT SOUND HERE INSTEAD OF THE IF STATEMENTS BECAUSE ITS NOT EFFICIENT AND IT WONT PLAY MORE THEN ONCE
+  if(hasWon){
+    resultsEl.innerText = `CONGRATS!! 😊 YOU WON $${totalWinAmount}`;
+    jackpotSound()
+  }
+  //THIS TRACKER WILL BE USED IN A SUPER RARE CAASE TO FIND OUT IF THERE IS A JACKPOT
+  if(tracker === 8){
+    resultsEl.innerText = `JACKPOTT!! 🥶 YOU WON $${totalWinAmount}`;
+  }
+  // NOW ITS TIME TO USE THE WIN VARIABLE WE USED ON TOP WHICH WOULDVE TURNED TO TRUE
+  //IF THERE WAS A WIN CONDITON BUT IF THERES NO WIN, ITLL STAY FALSE, AND IF ITS FALSE WE WILL 
+  //PUSH THE TEXT 'TRY AGAIN'
+  if (!hasWon) {
+    
+    resultsEl.innerText = `TRY AGAIN 🙁 YOU LOST $${parseInt(betAmount.innerText)}`;
+      //THE LINE BELOW WILL SUBTRACT THE BET AMOUNT FROM THE COINS YOU HAVE IN GAME, ONLY IF YOU DONT WIN
+      wallet.innerHTML = parseInt(wallet.innerHTML) - parseInt(betAmount.innerText);
+
+  }
+  //THIS WILL MAKE SURE THAT YOU DONT SEE A BET AMOUNT IF THE WALLET IS 0
+  if(wallet.innerHTML==='0'){
+    betAmount.innerText = '0'
+    betBtn.disabled= true;
+  }else{
+  betAmount.innerText = '10';}
+  
+}
+
+//FOR THE ABOVE FUNCTION ANOTHER WE COULD TRY WOULD BE BY USING IF STATEMENTS FOR ALL OF THEM BUT THAT WOULD BE A LOT OF CODE, SO USING LOOPS IS A BETTER IDEA
+
+
+
+
+//THIS FUNCTION IS USED TO INCREASE THE BETTING AMOUNT 
+ function increasebetting(){
+   if(parseInt(wallet.innerHTML) > parseInt(betAmount.innerText)){
+     betAmount.innerText = parseInt(betAmount.innerText) + 10;
+     betSound()
+     clickSound();
+  }
+  if(parseInt(wallet.innerHTML) <= parseInt(betAmount.innerText)){
+      betBtn.disabled = true;
+  }
+}
+
+//THIS EVENT LISTNER IS USED TO CALL INCREASEBETTING FUNCTION EVERYTIME BETBTN IS CLICKED
+  betBtn.addEventListener('click',increasebetting);
+
+
+
+  //THIS FUNCTION WILL TURN OFF THE GAME IF THE CONDITION INSIDE IS MET
+  function gameOver() {
+    if (parseInt(wallet.innerHTML) < 10) {
+      resultsEl.innerText = "YOU'RE OUT OF COINS 😢 RESET GAME TO TRY AGAIN!";
+      resetEl.disabled = false;
+      cashOut.disabled = true;
+      betBtn.disabled= true;
+      playBtn.disabled = true;
+      game = false;
+      addBackgroundAnimation();
+      loseSound()
+    }
+  }
+  
+  //THIS IS A FUNCTION FOR THE RESEST BUTTON, THAT WILL HELP RESTART THE GAME IN A LOOSE OR CASHED OUT SITAUTION
+  function resetButton() {
+    game = true;
+    wallet.innerHTML = '100';
+    betAmount.innerText = '10';
+
+    playBtn.disabled = false;
+    resetEl.disabled = true;
+    resultsEl.innerText = 'LETS PLAY!!😎';
+    betBtn.disabled= false;
+    cashOut.disabled = false;
+    removeBackgroundAnimation()
+    startIcons()
+    resetSound()
+    clickSound()
+  }
+  //THIS BUTTON WILL CALL THE RESETBUTTON FUNCTION EVERYTIME ITS CLICKED
+  resetEl.addEventListener('click', resetButton);
+  
+  
+  
+  
+  //THIS FUNCTION IS USED FOR PLAYING THE CASHOUT SOUND, IT ALSO DISABLES IT IF ITS CLICKED ONCE 
+  function cashOutSound() {
+ 
     const cashOutSound = new Audio('sounds/ESM_Vibrant_Cash_Register_Open_Positive_Game_Open_Arcade_Cartoon_Quirky_Comedy_Comedic_Kid_Childish_Fun_Bouncy.mp3'); 
     cashOutSound.play();
     if(cashOut.disabled === false){
@@ -137,64 +333,45 @@ function cashOutSound() {
     
 }
 
-function clickSound() {
-    const clickSound = new Audio('sounds/MouseClick_SFXB.4113.mp3'); 
-    clickSound.play();
-}
-
-const musicSound = new Audio('sounds/CasinoSlotMachine_SFXB.21.mp3'); 
-musicSound.loop = true;
-function playMusicSound() {
-    
-    musicSound.play();
-}
-function musicButtonClick() {
-    if (musicSound.paused) {
-      musicSound.play();
-      clickSound();
-    } else {
-      musicSound.pause();
-      clickSound();
-    }
-    
-  }
-  music.addEventListener('click',musicButtonClick);
-
-
-function musicButtonClickforSpin() {
-    clickSound();
-    spinSound();
-  }
 function musicButtonClickforCashOut() {
-    clickSound();
-    cashOutSound();
+  clickSound();
+  cashOutSound();
   
-  }
-  
-  cashOut.addEventListener('click',musicButtonClickforCashOut);
+}
 
+//THIS EVENT LISTENER CALLS THE FUNCTION BELOW EVERYTIME ITS CLICKED 
+cashOut.addEventListener('click',function(){
+  stopGlowing()
+  musicButtonClickforCashOut()
+  resultsEl.innerText = `BET REMOVED AND CASHED OUT $${wallet.innerHTML} 🤑 RESET GAME TO PLAY AGAIN!`;
+  wallet.innerHTML = '0';
+  betAmount.innerText ='0';
+  betBtn.disabled = true;
+  resetEl.disabled = false;
+  playBtn.disabled = true;
+})
 
-playBtn.addEventListener('click',musicButtonClickforSpin);
-
-function gameOver() {
-    if (parseInt(wallet.innerHTML) < 10) {
-      resultsEl.innerText = 'YOUR OUT OF MONEY :( RESET GAME TO TRY AGAIN!';
-      resetEl.disabled = false;
-      cashOut.disabled = true;
-      playBtn.disabled = true;
-      game = false;
-      loseSound()
+//THIS FUNCTION WILL STOP ALL WIN GLOWINGS WHENEVER ITS NEEDED, THE FUNCTION REMOVES THE CLASS OF WIN FROM ALL THE BOXES EVEN IF THEY DONT HAVE IT
+function stopGlowing(){
+    for(let i = 0; i<boxes.length ;i++){
+      boxes[i].classList.remove('win');
     }
+}
+//BOTH OF THE FUNCTIONS BELOW ARE ADDING AND REMOVING THE SAME SPINANIMATION WE USED ON THE PLAYBTN
+function boxesGlow(){
+  for(let i = 0; i<boxes.length ;i++){
+    boxes[i].classList.add('spinanimation');
   }
-  
-  playBtn.addEventListener('click', function() {
-    
-      playBtn.disabled = true; // Disable the spin button
-      setTimeout(function() {
-        wonOrTryAgain();
-        playBtn.disabled = !game; // Enable the spin button if the game is not over
-        gameOver();
-    }, 1800);
-    }
-  );
+}
+
+function stopBoxesGlow(){
+  for(let i = 0; i<boxes.length ;i++){
+    boxes[i].classList.remove('spinanimation');
+  }
+}
+function removeBoxesImage(){
+  for(let i = 0; i<boxes.length ;i++){
+    boxes[i].style.backgroundImage = 'none';
+  }
+}
 
